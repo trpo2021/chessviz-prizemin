2ab94d3531bed221c32cdcc80ab72eaef040e9a9
leo <lprizemin@mail.ru>
2021-04-11 17:48:30 +0700

    Create printBoard

---
+++ src/libchessviz/printBoard.cpp
@@ -0,0 +1,15 @@
+#include <libchessviz/printBoard.h>
+#include <iostream>
+
+using namespace std;
+void printBoard(char ch[][8])
+{
+  for (int i = 0; i < 8; i++) 
+  {
+		for (int j = 0; j < 8; j++) 
+		{
+			cout << ch[i][j] << " ";
+		}
+		cout << endl;
+  }
+}
