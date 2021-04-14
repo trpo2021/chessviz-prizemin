56b113249d868b8a3f38d01d7a8e13b449e27623
leo <lprizemin@mail.ru>
2021-04-14 18:25:56 +0700

    Fix codestyle

--- src/libchessviz/checkMovePeshka.h
+++ src/libchessviz/checkMovePeshka.h
@@ -1,2 +1,8 @@
 #pragma once
-int checkMovePeshka(char ch[][8], int int_start1, int start2, int finish2, int int_finish1, int BorW);
+int checkMovePeshka(
+        char ch[][8],
+        int int_start1,
+        int start2,
+        int finish2,
+        int int_finish1,
+        int BorW);
