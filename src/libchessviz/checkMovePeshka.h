7750c1292f6ca27b8c738238d35562a3f9089816
prizemin <78478873+prizemin@users.noreply.github.com>
2021-04-27 02:10:22 +0700

    Revert "Feedback"

--- src/libchessviz/checkMovePeshka.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-int checkMovePeshka(
-        char ch[][8],
-        int int_start1,
-        int start2,
-        int finish2,
-        int int_finish1,
-        int BorW);
--- src/libchessviz/figureMove.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-void figureMove(
-        char ch[][8], int int_start1, int start2, int finish2, int int_finish1);
